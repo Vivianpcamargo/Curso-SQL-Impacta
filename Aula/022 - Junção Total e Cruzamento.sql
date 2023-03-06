d2df0edd84ee01236da44df02d3bd5a7853496ab
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2023-03-06 14:52:09 -0300

    update: Aula 22

---
+++ Aula/022 - Junção Total e Cruzamento.sql
@@ -0,0 +1,20 @@
+USE SisDep;
+
+SELECT
+    F.NomeFuncionario, F.Admissao, D.NomeDependente, D.NascimentoDependente
+FROM Funcionario AS F
+FULL JOIN Dependente AS D
+ON F.idMatricula = D.idMatricula
+ORDER BY F.NomeFuncionario;
+
+SELECT * FROM Depto;
+
+SELECT * FROM Projeto;
+
+SELECT
+    D.NomeDepartamento,
+    P.NomeProjeto,
+    P.DataInicio,
+    P.DataTermino
+FROM Depto AS D
+CROSS JOIN Projeto AS P;
