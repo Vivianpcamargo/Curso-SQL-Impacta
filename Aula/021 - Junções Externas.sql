a598769f16ce37ce393a9f4b0052a0479b443ca0
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2023-03-06 14:39:57 -0300

    update: Aula 21

---
+++ Aula/021 - Junções Externas.sql
@@ -0,0 +1,19 @@
+USE SisDep;
+
+SELECT 
+    F.NomeFuncionario,
+    D.NomeDependente,
+    D.NascimentoDependente
+FROM Funcionario AS F
+LEFT OUTER JOIN Dependente AS D
+ON F.idMatricula = D.idMatricula
+ORDER BY F.NomeFuncionario ASC;
+
+SELECT 
+    F.NomeFuncionario,
+    C.NomeCargo,
+    F.Salario
+FROM Funcionario AS F
+RIGHT JOIN Cargo AS C
+ON F.idCargo = C.idCargo
+WHERE F.NomeFuncionario IS NULL;
